e397181909ff10f2b1f96e7b3b22e9ca92b44243
wisher-18 <113600130+wisher-18@users.noreply.github.com>
2023-03-22 01:08:38 +0530

    21/03 3rd commit

---
+++ ForEachExample.java
@@ -0,0 +1,13 @@
+
+public class ForEachExample {
+
+	public static void main(String[] args) {
+		String [] goat = {"Virat", "Sachin", "Dhoni", "Ronaldo", "Messi"};
+		for(String n: goat)
+		{
+			System.out.println(n);
+		}
+
+	}
+
+}
