1f3cd8e841e962bca680d29cc0baa060ebb2df3e
wisher-18 <113600130+wisher-18@users.noreply.github.com>
2023-03-30 18:09:22 +0530

    30/03 Commit 1st

---
+++ Assisgn.java
@@ -0,0 +1,13 @@
+
+public class Assisgn {
+
+	public static void main(String[] args) {
+		int a = 15;// declaration and assignment
+		int b = 10;
+		
+		int c = a+b;// expression
+		System.out.println(c);
+
+	}
+
+}
---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte a= 100;
+		short b = 150;
+		int c = 200;
+		long l = 300;
+		
+		char ch = 'a';
+		String name = "Bhushan";
+		
+		float z = 10.0f;
+		double d = 100.200;
+		
+		boolean flag = true;
+		
+		System.out.println("Byte: "+a+"\nShort: "+b+"\nInt: "+c+"\nLong: "+l+"\n\nChar: "+ch+"\nString: "+name+"\nFloat: "+z+
+				"\nDouble: "+d+"\n\nBoolean: "+flag);
+		
+		
+
+	}
+
+}
