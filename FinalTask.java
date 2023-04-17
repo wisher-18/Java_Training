6a213390b643b9c91cfc9e72541bafbb96ad8ae6
wisher-18 <113600130+wisher-18@users.noreply.github.com>
2023-04-18 00:27:54 +0530

    Final Task 18/04

---
+++ FinalTask.java
@@ -0,0 +1,20 @@
+
+final class Example
+{
+	final void method()
+	{
+		System.out.println("This is final method...");
+	}
+}
+
+
+public class FinalTask {
+
+	public static void main(String[] args) {
+		Example e1 = new Example();
+		e1.method();
+		
+
+	}
+
+}
